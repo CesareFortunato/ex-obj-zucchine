144df9e22399a9794e803c3731b806df2f2ea631
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-15 12:42:22 +0100

    feat: 2 ways to sum peso

--- script.js
+++ script.js
@@ -9,31 +9,37 @@
 // creo obj zucchine
 
 const listaZucchine = [
-    { varietà: "varietà1", peso: 0.5, lunghezza: 5 },
-    { varietà: "varietà2", peso: 1,   lunghezza: 5 },
-    { varietà: "varietà3", peso: 0.8, lunghezza: 5 },
-    { varietà: "varietà4", peso: 0.6, lunghezza: 5 },
-    { varietà: "varietà5", peso: 0.5, lunghezza: 5 },
-    { varietà: "varietà6", peso: 0.9, lunghezza: 5 },
-    { varietà: "varietà7", peso: 0.4, lunghezza: 5 },
-    { varietà: "varietà8", peso: 0.3, lunghezza: 5 },
-    { varietà: "varietà9", peso: 0.6, lunghezza: 5 },
-    { varietà: "varietà10", peso: 1.2, lunghezza: 5 }
+    { 'varieta': "varietà1", peso: 0.5, lunghezza: 15 },
+    { 'varieta': "varietà2", peso: 1, lunghezza: 17 },
+    { 'varieta': "varietà3", peso: 0.8, lunghezza: 4 },
+    { 'varieta': "varietà4", peso: 0.6, lunghezza: 6 },
+    { 'varieta': "varietà5", peso: 0.5, lunghezza: 9 },
+    { 'varieta': "varietà6", peso: 0.9, lunghezza: 15 },
+    { 'varieta': "varietà7", peso: 0.4, lunghezza: 24 },
+    { 'varieta': "varietà8", peso: 0.3, lunghezza: 8 },
+    { 'varieta': "varietà9", peso: 0.6, lunghezza: 11 },
+    { 'varieta': "varietà10", peso: 1.2, lunghezza: 6 }
 
 ];
 
 
 //SYSTEM
 
-let pesoTotale = 0;
+// variabile per accumulare il totale dei pesi
+
+let pesoTotale = 0;  
+
+// ciclo for per aggiungere il peso di ogni oggetto nell'array alla variabile settata prima
 
 for (let i = 0; i < listaZucchine.length; i++) {
+
+    // si può anche scrivere per esteso:  pesoTotale = pesoTotale + listaZucchine[i].peso;
 
     pesoTotale += listaZucchine[i].peso;
 
     console.log(listaZucchine[i].peso);
 
-        
+
 }
 
-console.log("Questa è la somma totale: " + pesoTotale +" kg");
+console.log("Questa è la somma totale: " + pesoTotale + " kg");
